f7c877cfc65ec1442ece8269fbafe57a1efbf9e5
Valentin Parafioriti <vlp1025@rit.edu>
2022-11-12 12:59:38 -0500

    added extra feature

--- client/maker.jsx
+++ client/maker.jsx
@@ -1,71 +1,73 @@
 const helper = require('./helper.js');
-
+ 
 const handleDomo = (e) => {
     e.preventDefault();
     helper.hideError();
-
+ 
     const name = e.target.querySelector('#domoName').value;
     const age = e.target.querySelector('#domoAge').value;
     const _csrf = e.target.querySelector('#_csrf').value;
     const health = e.target.querySelector('#domoHealth').value;
-
-
+ 
+ 
     if (!name || !age) {
         helper.handleError('All fields are required!');
         return false;
     }
-
+ 
     helper.sendPost(e.target.action, { name, age, health, _csrf }, loadDomosFromServer);
-
+ 
     return false;
 };
-
+ 
 const DomoForm = (props) => {
     return (
-        <form id="domoForm"
-            onSubmit={handleDomo}
-            name="domoForm"
-            action="/maker"
-            method="POST"
-            className="domoForm"
-        >
-            <label htmlFor="name">Name: </label>
-            <input type="text" id="domoName" name="name" placeholder="Domo Name" />
-            <label htmlFor="age">Age: </label>
-            <input type="number" id="domoAge" name="age" min="0" />
-            <label htmlFor="health">Health: </label>
-            <input type="number" id="domoHealth" name="health" min="0" max="10" />
-            <input type="hidden" id="_csrf" name="_csrf" value={props.csrf} />
-            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
-
-{/* 
-            <form ref='uploadForm'
+        <div>
+            <form id="domoForm"
+                onSubmit={handleDomo}
+                name="domoForm"
+                action="/maker"
+                method="POST"
+                className="domoForm"
+            >
+                <label htmlFor="name">Name: </label>
+                <input type="text" id="domoName" name="name" placeholder="Domo Name" />
+                <label htmlFor="age">Age: </label>
+                <input type="number" id="domoAge" name="age" min="0" />
+                <label htmlFor="health">Health: </label>
+                <input type="number" id="domoHealth" name="health" min="0" max="10" />
+                <input type="hidden" id="_csrf" name="_csrf" value={props.csrf} />
+                <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+ 
+ 
+            </form>
+ 
+            <form
                 id='uploadForm'
                 action='/upload'
                 method='post'
                 encType="multipart/form-data">
                 <input type="file" name="sampleFile" />
+                <input type="hidden" id="_csrf" name="_csrf" value={props.csrf} />
                 <input type='submit' value='Upload!' />
             </form>
-
-            <form ref='retrieveForm'
+ 
+            <form
                 id='retrieveForm'
                 action='/retrieve'
                 method='get'>
-                <label for='fileName'>Retrieve File By ID: </label>
+                <label htmlFor='fileName'>Retrieve File By ID: </label>
                 <input name='_id' type='text' />
                 <input type='submit' value='Retrieve!' />
             </form>
-
-            <section id="messages"></section> */}
-
-
-        </form>
-
-
+ 
+            <section id="messages"></section>
+ 
+        </div>
+ 
     );
 };
-
+ 
 const DomoList = (props) => {
     if (props.domos.length === 0) {
         return (
@@ -74,25 +76,27 @@ const DomoList = (props) => {
             </div>
         );
     }
-
+ 
     const domoNodes = props.domos.map(domo => {
+        const imageUrl = `/retrieve?_id=${domo.imgId}`;
         return (
             <div key={domo._id} className="domo">
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className='domoFace' />
                 <h3 className='domoName'>Name: {domo.name} </h3>
                 <h3 className='domoAge'>Age: {domo.age} </h3>
                 <h3 className='domoHealth'>Health: {domo.health} </h3>
+                {/* <img src={imageUrl} /> */}
             </div>
         );
     });
-
+ 
     return (
         <div className='domoList'>
             {domoNodes}
         </div>
     );
 };
-
+ 
 const loadDomosFromServer = async () => {
     const response = await fetch('/getDomos');
     const data = await response.json();
@@ -101,23 +105,30 @@ const loadDomosFromServer = async () => {
         document.getElementById('domos')
     );
 };
-
+ 
 const init = async () => {
     const response = await fetch('/getToken');
     const data = await response.json();
-
+ 
     ReactDOM.render(
         <DomoForm csrf={data.csrfToken} />,
         document.getElementById('makeDomo')
     );
-
+ 
     ReactDOM.render(
         <DomoList domos={[]} />,
         document.getElementById('domos')
     );
-
+ 
+    // const domos = document.getElementById('domos');
+    // const image = document.createElement('img');
+    // const imageId = '636fdb48eae0701545149ec7';
+    // image.src = `/retrieve?_id=${imageId}`;
+    // domos.appendChild(image);
+ 
     loadDomosFromServer();
 };
-
+ 
 window.onload = init;
-
+ 
+ 
--- server/models/filestore.js
+++ server/models/filestore.js
@@ -1,28 +1,13 @@
-/* This model file defines the mongoose schema and model for a
-   database collection that can store files. The format is based
-   on the file object that is sent to our server by the browser
-   when the user uploads a file.
-*/
+
 const mongoose = require('mongoose');
 
 const FileSchema = new mongoose.Schema({
-  /* The name attribute that we recieve and will store defines
-     the actual filename of the file uploaded. This will be equal
-     to whatever the name of the file is on the users computer.
-     The name will include the file extension.
-  */
+
   name: {
     type: String,
   },
 
-  /* The data attribute is the actual data within the file. It
-     will be recieved by the server as a buffer of binary
-     information. A buffer is similar to an array. A collection
-     of information. If an image is uploaded, the buffer would
-     store metadata about the image as well as the pixel information.
-     If a text file is uploaded, it will contain metadata and
-     the character information.
-  */
+
   data: {
     type: Buffer,
   },
@@ -32,12 +17,7 @@ const FileSchema = new mongoose.Schema({
     type: Number,
   },
 
-  /* The mimetype defines the actual type of file that is being
-     sent and stored. MIME types tell things like the browser how
-     to interpret the binary information stored in the data
-     attribute. For example, the data format of a png will be
-     very different from the data format of an mp3.
-  */
+
   mimetype: {
     type: String,
   },
